c4c7d9b6c4dc443f63bcf4280ddc0da6ff242284
status102 <status102@outlook.com>
2023-05-02 20:17:32 +0800

    fix: 修复森都房区名字错误，导致无法上传至HouseHelper

--- WannaHome/Data/Territory.cs
+++ WannaHome/Data/Territory.cs
@@ -7,16 +7,16 @@ namespace WannaHome.Data
 	{
 		public static List<Territory> Territories = new()
 		{
-			new (){id = 339, fullName = "海雾村", nickName = "海"},
-			new (){id = 340, fullName = "薰衣草苗园", nickName = "森"},
-			new (){id = 341, fullName = "高脚孤丘", nickName = "沙"},
-			new (){id = 641, fullName = "白银乡", nickName = "白"},
-			new (){id = 979, fullName = "穹顶皓天", nickName = "天", sell = false}
+			new (){Id = 339, FullName = "海雾村", NickName = "海"},
+			new (){Id = 340, FullName = "薰衣草苗圃", NickName = "森"},
+			new (){Id = 341, FullName = "高脚孤丘", NickName = "沙"},
+			new (){Id = 641, FullName = "白银乡", NickName = "白"},
+			new (){Id = 979, FullName = "穹顶皓天", NickName = "天"}// , sell = false
 		};
-		public static Dictionary<ushort, Territory> TerritoriesMap => Territories.ToDictionary(i => i.id);
-		public ushort id { get; init; }
-		public string fullName { get; init; } = "";
-		public string nickName { get; init; } = "";
-		public bool sell { get; init; } = true;
+		public static Dictionary<ushort, Territory> TerritoriesMap => Territories.ToDictionary(i => i.Id);
+		public ushort Id { get; init; }
+		public string FullName { get; init; } = "";
+		public string NickName { get; init; } = "";
+		public bool OnSell { get; init; } = true;
 	}
 }
